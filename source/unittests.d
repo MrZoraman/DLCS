83361ed2814a0c7b13f607a4001016e2a6af56f5
Zora <MrZoraman@Gmail.com>
2015-09-29 20:27:02 -0600

    better unit tests

--- source/unittests.d
+++ source/unittests.d
@@ -1,93 +1,65 @@
-import std.stdio;
-import std.string;
-
-import dlcs.dlcs;
-
-interface ICommand
+unittest
 {
-    public void execute(string[] preArgs, string[] args);
-}
-
-class CommandTester : ICommand
-{
-    private string _str;
+    import std.stdio;
+    import std.string;
+    import std.exception;
     
-    public this(string str)
+    import dlcs.dlcs;
+    import dlcs.parsing;
+    
+    class TestCommand
     {
-        _str = str;
-    }
-    
-    public override void execute(string[] preArgs, string[] args)
-    {
-        writeln("Command executed!");
-        writeln("args.length: ", args.length);
-        writeln("preArgs.length: ", preArgs.length);
-        writeln("PreArgs: ", preArgs);
-        writeln("args: ", args);
-        writeln("Message: ", _str);
-    }
-    
-    public override string toString() const
-    {
-        return "I am CommandTester: " ~  _str;
-    }
-}
-
-void main()
-{
-
-    CommandSystem!ICommand lcs = new CommandSystem!ICommand();
-    
-    lcs.registerCommand("a", new CommandTester("Hello!"));
-    lcs.registerCommand("a b", new CommandTester("Wah!"));
-    lcs.registerCommand("b", new CommandTester("Hello again!"));
-    lcs.registerCommand("b c|d", new CommandTester("woah!"));
-    lcs.registerCommand("e f|g h", new CommandTester("dayum!"));
-    lcs.registerCommand("i j|{k l} m", new CommandTester("woot!"));
-    lcs.registerCommand("n * o", new CommandTester("the world is broken!"));
-    lcs.registerCommand("p * q * s", new CommandTester("I don't even know at this point!"));
-    lcs.registerCommand("t * * v", new CommandTester("why?!"));
-    lcs.registerCommand("t * w v", new CommandTester("uwot"));
-//            
-//            lcs.registerCommand("x } oops", new CommandTester("oops"));
-//            lcs.registerCommand("y {oops {", new CommandTester("woops"));
-//            lcs.registerCommand("z { lady dady da", new CommandTester("dang"));
-//        
-    lcs.registerCommand("{home set}|sethome", new CommandTester("woot"));
-    lcs.registerCommand("home", new CommandTester("weiew"));
-    lcs.registerCommand("homenwinnigish", new CommandTester("Chicken n' winnigish"));
-//            lcs.registerCommand("home", new CommandTester("weiew"));
-    
-            lcs.registerCommand("", new CommandTester("wot"));
-    
-    lcs.unknownCommand = new CommandTester("I have no clue what that is");
-    
-    lcs.printCommandTree(stdout);
-
-
-    bool done = false;
-    while(!done)
-    {
-        write("> ");
-        string input = chomp(readln());
-        
-        writeln("INPUT: ", input);
-        
-        if(input == "exit")
+        this(int id)
         {
-            done = true;
+            this.id = id;
         }
-        else
-        {
-            CommandResult!ICommand result = lcs.getCommand(input);
-            if(result.command is null)
-            {
-                writeln("Command not found!");
-            }
-            else
-            {
-                result.command.execute(result.preArgs, result.args);
-            }
-        }
+        
+        public int id;
     }
+
+    CommandSystem!TestCommand lcs = new CommandSystem!TestCommand();
+    
+    lcs.registerCommand("", new TestCommand(0));
+    lcs.registerCommand("a", new TestCommand(1));
+    lcs.registerCommand("a b", new TestCommand(2));
+    lcs.registerCommand("b", new TestCommand(3));
+    lcs.registerCommand("b c|d", new TestCommand(4));
+    lcs.registerCommand("e f|g h", new TestCommand(5));
+    lcs.registerCommand("i j|{k l} m", new TestCommand(6));
+    lcs.registerCommand("n * o", new TestCommand(7));
+    lcs.registerCommand("p * q * s", new TestCommand(8));
+    lcs.registerCommand("t * * v", new TestCommand(9));
+    lcs.registerCommand("t * w v", new TestCommand(10));
+    
+    lcs.unknownCommand = new TestCommand(-1);
+    
+    assert(lcs.getCommand("").command.id == 0);
+    assert(lcs.getCommand("a").command.id == 1);
+    assert(lcs.getCommand("a b").command.id == 2);
+    assert(lcs.getCommand("b").command.id == 3);
+    assert(lcs.getCommand("b c").command.id == 4);
+    assert(lcs.getCommand("b d").command.id == 4);
+    assert(lcs.getCommand("e f h").command.id == 5);
+    assert(lcs.getCommand("e g h").command.id == 5);
+    assert(lcs.getCommand("i j m").command.id == 6);
+    assert(lcs.getCommand("i k l m").command.id == 6);
+    assert(lcs.getCommand("n ? o").command.id == 7);
+    assert(lcs.getCommand("p ? q ? s").command.id == 8);
+    assert(lcs.getCommand("t ? ? v").command.id == 9);
+    assert(lcs.getCommand("t ? w v").command.id == 10);
+    assert(lcs.getCommand("err").command.id == -1);
+    
+    assert(lcs.getCommand("a 1 2 3").args == ["1", "2", "3"]);
+    
+    assert(lcs.getCommand("t 1 2 v").preArgs == ["1", "2"]);
+    
+    CommandResult!TestCommand c = lcs.getCommand("n ! o @");
+    assert(c.command.id == 7);
+    assert(c.args == ["@"]);
+    assert(c.preArgs == ["!"]);
+    
+    TestCommand badCommand = new TestCommand(-2);
+    assertThrown!ParseFailException(lcs.registerCommand("{i am error{", badCommand));
+    assertThrown!ParseFailException(lcs.registerCommand("{i am also error", badCommand));
+    assertThrown!ParseFailException(lcs.registerCommand("}i am bad as well", badCommand));
 }
